594c7be40598b68e0c09da357c2f50a335724ed7
CynthiaKamau <cynthia.kamau@students.jkuat.ac.ke>
2019-11-15 11:16:26 +0300

    Added Theme Model

--- api/models/role.py
+++ api/models/role.py
@@ -4,8 +4,8 @@ from api.models.database import BaseModel
 class Role(BaseModel.db.Model):
     AppDb = BaseModel.db
     id = AppDb.Column(AppDb.Integer, primary_key=True)
-    code = AppDb.Column(AppDb.String(20), nullable=False, unique=True, index=True)
-    name = AppDb.Column(AppDb.String(50), nullable=False)
+    code = AppDb.Column(AppDb.String(20), nullable=False, unique=True, index=True) #id on badge
+    name = AppDb.Column(AppDb.String(50), nullable=False) #predifined job
     description = AppDb.Column(AppDb.String(255), nullable=True)
 
     # relationship(s) defined here
