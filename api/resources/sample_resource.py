317861360e338b01c3644075b342bec35ebf41af
CynthiaKamau <cynthiakamau54@gmail.com>
2019-11-19 09:39:26 +0300

    Removed Relationship in Lab Model

--- api/resources/sample_resource.py
+++ api/resources/sample_resource.py
@@ -1,9 +1,11 @@
+from flask import request, jsonify
 from api.resources.base_resource import BaseResource
 from flask_restful import Resource
 from api.models.sample import Sample
 
 
+
 class SampleResource (BaseResource):
     def get(self):
         samples = Sample.query.all()
-        return jsonify({'samples' : samples})
+        return jsonify( json_list = Sample.query.all() )
