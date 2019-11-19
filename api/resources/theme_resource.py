0c11518e7bb40ecbf8527c4c315e1af143fd8287
jeffkim <jeffkim207@gmail.com>
2019-11-19 16:26:13 +0300

    Refined module to include static methods for redudant code.

--- api/resources/theme_resource.py
+++ api/resources/theme_resource.py
@@ -18,17 +18,13 @@ class ThemeResource(BaseResource):
         return BaseResource.send_json_message(data, 200)
 
     def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('code')
-        parser.add_argument('name', required=True)
-
-        args = parser.parse_args()
+        args = ThemeResource.theme_parser()
         code = args['code']
         name = args['name']
 
         if not Theme.theme_exists(name):
             try:
-                theme = Theme(name=name)
+                theme = Theme(code=code, name=name)
                 BaseModel.db.session.add(theme)
                 BaseModel.db.session.commit()
                 return BaseResource.send_json_message("Added new theme", 201)
@@ -41,43 +37,48 @@ class ThemeResource(BaseResource):
         return BaseResource.send_json_message("Theme already exists", 500)
 
     def put(self, id):
-        parser = reqparse.RequestParser()
-        parser.add_argument('name', required=True)
-        parser.add_argument('code')
-
-        args = parser.parse_args()
+        args = ThemeResource.theme_parser()
         name = args['name']
         code = args['code']
 
-        theme = BaseModel.db.session.query(Theme).get(id)
+        theme = ThemeResource.get_theme(id)
 
-        if name != theme.name:
-            if Theme.theme_exists(name):
-                current_app.logger.error("Error while adding theme :> Duplicate records")
-                return BaseResource.send_json_message("Theme already exists", 500)
+        if theme is not None:
+            if name != theme.name or code != theme.code:
+                try:
+                    theme.code = code
+                    theme.name = name
+                    BaseModel.db.session.commit()
+                    return BaseResource.send_json_message("Updated theme", 202)
 
-            try:
-                theme.code = code
-                theme.name = name
-                BaseModel.db.session.commit()
-                return BaseResource.send_json_message("Updated theme", 202)
+                except Exception as e:
+                    current_app.logger.error(e)
+                    BaseModel.db.session.rollback()
+                    return BaseResource.send_json_message("Error while adding theme. Another theme has that name or "
+                                                          "code", 500)
 
-            except Exception as e:
-                current_app.logger.error(e)
-                BaseModel.db.session.rollback()
-                return BaseResource.send_json_message("Error while adding theme", 500)
-        return BaseResource.send_json_message("No changes made", 200)
+            return BaseResource.send_json_message("No changes made", 304)
+        return BaseResource.send_json_message("Theme not found", 404)
 
     def delete(self, id):
-
-        theme = BaseModel.db.session.query(Theme).get(id)
-        print(theme.name)
+        theme = ThemeResource.get_theme(id)
 
         if not theme:
-            return BaseResource.send_json_message("No theme with that name")
+            return BaseResource.send_json_message("Theme not found", 404)
 
         BaseModel.db.session.delete(theme)
         BaseModel.db.session.commit()
         return BaseResource.send_json_message("Theme deleted", 200)
 
+    @staticmethod
+    def theme_parser():
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', required=True)
+        parser.add_argument('code')
 
+        args = parser.parse_args()
+        return args
+
+    @staticmethod
+    def get_theme(theme_id):
+        return BaseModel.db.session.query(Theme).get(theme_id)
