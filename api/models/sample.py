fcf06c0260bb5163d4fbb914f6620144a325cfea
jeffkim <jeffkim207@gmail.com>
2019-12-10 14:22:25 +0300

    Added quantity type to allow for different sample amounts to be marked with the appropriate quantity. Eg ml for liquids or g for weight

--- api/models/sample.py
+++ api/models/sample.py
@@ -17,22 +17,23 @@ class Sample(BaseModel.db.Model):
     barcode = AppDb.Column(AppDb.String(100), nullable=True)  # todo: changed to not nullable when module is done
     analysis = AppDb.Column(AppDb.String(100), nullable=True)
     temperature = AppDb.Column(AppDb.DECIMAL(5, 2), nullable=False)  # Default = Celsius
-    amount = AppDb.Column(AppDb.Integer, nullable=False)  # todo set a default value
+    amount = AppDb.Column(AppDb.Integer, nullable=False)
+    quantity_type = AppDb.Column(AppDb.String, AppDb.ForeignKey('quantity_type.id', ondelete='SET NULL'), nullable=True)
     code = AppDb.Column(AppDb.String, nullable=True)
 
     # relationship(s)
     user = AppDb.relationship('User', backref='sample', lazy=True)
     publication = AppDb.relationship('Publication', backref='sample', lazy=True)
     box = AppDb.relationship('Box', backref='sample', lazy=True)
-    # todo: Add relationship between box and sample
+    quantity = AppDb.relationship('QuantityType', backref='quantity_type', lazy=True)
 
     def __repr__(self):
         return '<< Sample: (type={0} || desc={1} || project={2} || barcode={3} || species={4} ||' \
-            'box={5} || retention={6} || amount={7} || code={8} || location={9} || owner={10}'  \
-            'analysis={11} || temperature={12} ) >>' \
-        .format(self.sample_type, self.sample_description,
-            self.project, self.barcode, self.animal_species,
-            self.box_id, self.retention_period, self.amount, self.code,
-            # not represented
-            self.location_collected, self.project_owner,
-            self.analysis, self.temperature)
+               'box={5} || retention={6} || amount={7} || amount={8} || code={9} || location={10} || owner={11}' \
+               'analysis={12} || temperature={13} ) >>' \
+            .format(self.sample_type, self.sample_description,
+                    self.project, self.barcode, self.animal_species,
+                    self.box_id, self.retention_period, self.amount, self.quantity_type, self.code,
+                    # not represented
+                    self.location_collected, self.project_owner,
+                    self.analysis, self.temperature)
