1c9fdd736452654eb3016272afcf0927720e9f52
jeffkim <jeffkim207@gmail.com>
2019-12-11 15:16:44 +0300

    Modified resource; Added logging of user actions, restricted Create, Update and Delete of records to authenticated users, request variables now sent as header params rather than in url.

--- api/resources/rack_resource.py
+++ api/resources/rack_resource.py
@@ -1,9 +1,11 @@
-from flask import current_app
+from flask import current_app, request
+from flask_jwt_extended import jwt_required
 from flask_restful import fields, marshal, reqparse
 
 from api.models.database import BaseModel
 from api.models.rack import Rack
 from api.resources.base_resource import BaseResource
+from api.utils import format_and_lower_str, log_create, log_duplicate, log_update, log_delete, non_empty_string
 
 
 class RackResource(BaseResource):
@@ -14,35 +16,44 @@ class RackResource(BaseResource):
     }
 
     def get(self):
-        racks = Rack.query.all()
-        data = marshal(racks, self.fields)
-        return BaseResource.send_json_message(data, 200)
+        if request.headers.get('code') is not None:
+            code = format_and_lower_str(request.headers['code'])()
+            rack = RackResource.get_rack(code)
+            data = marshal(rack, self.fields)
+            return BaseResource.send_json_message(data, 200)
+        else:
+            racks = Rack.query.all()
+            data = marshal(racks, self.fields)
+            return BaseResource.send_json_message(data, 200)
 
+    @jwt_required
     def post(self):
         args = RackResource.rack_parser()
         chamber = int(args['chamber'])
         number = int(args['number'])
-        code = (args['code'])
+        code = format_and_lower_str(args['code'])()
 
-        if not Rack.rack_exists(number):
+        if not Rack.rack_exists(code):
             try:
                 rack = Rack(chamber_id=chamber, number=number, code=code)
                 BaseModel.db.session.add(rack)
                 BaseModel.db.session.commit()
+                log_create(rack)
                 return BaseResource.send_json_message("Created new rack", 201)
 
             except Exception as e:
                 current_app.logger.error(e)
                 BaseModel.db.session.rollback()
-                return BaseResource.send_json_message("Error while adding tray", 500)
-        current_app.logger.error("Error while adding tray :> Duplicate records")
-        return BaseResource.send_json_message("Tray already exists", 500)
+                return BaseResource.send_json_message("Error while adding rack", 500)
+        log_duplicate()
+        return BaseResource.send_json_message("Rack already exists", 500)
 
-    def put(self, code):
+    @jwt_required
+    def put(self):
         args = RackResource.rack_parser()
         chamber = int(args['chamber'])
         number = int(args['number'])
-        code = (args['code'])
+        code = format_and_lower_str(args['code'])()
 
         rack = RackResource.get_rack(code)
 
@@ -53,21 +64,25 @@ class RackResource(BaseResource):
                     rack.number = number
                     rack.code = code
                     BaseModel.db.session.commit()
+                    log_update(rack, rack)  # todo: log old update
                     return BaseResource.send_json_message("Updated rack", 202)
                 except Exception as e:
                     current_app.logger.error(e)
                     BaseModel.db.session.rollback()
-                    return BaseResource.send_json_message("Error while adding tray. Another tray has that number", 500)
+                    return BaseResource.send_json_message("Error while adding rack. Another rack has that number", 500)
             return BaseResource.send_json_message("No changes made", 304)
         return BaseResource.send_json_message("Rack not found", 404)
 
-    def delete(self, code):
+    @jwt_required
+    def delete(self):
+        code = format_and_lower_str(request.headers['code'])()
         rack = RackResource.get_rack(code)
 
         if rack is None:
             return BaseResource.send_json_message("Rack not found", 404)
         BaseModel.db.session.delete(rack)
         BaseModel.db.session.commit()
+        log_delete(rack)
         return BaseResource.send_json_message("Rack deleted", 200)
 
     @staticmethod
@@ -75,7 +90,7 @@ class RackResource(BaseResource):
         parser = reqparse.RequestParser()
         parser.add_argument('chamber', required=True)
         parser.add_argument('number', required=True)
-        parser.add_argument('code', required=True)
+        parser.add_argument('code', required=True, type=non_empty_string)
 
         args = parser.parse_args()
         return args
