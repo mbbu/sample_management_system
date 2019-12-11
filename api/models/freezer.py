41a5b6bc6706502e8b5cbe69893af150459c143e
jeffkim <jeffkim207@gmail.com>
2019-12-11 13:23:40 +0300

    Modified resource; Added logging of user actions, restricted Create, Update and Delete of records to authenticated users, request variables now sent as header params rather than in url. Modified representation of freezer.

--- api/models/freezer.py
+++ api/models/freezer.py
@@ -7,11 +7,11 @@ class Freezer(BaseModel.db.Model):
     laboratory_id = AppDb.Column(AppDb.Integer, AppDb.ForeignKey('laboratory.id', ondelete='SET NULL'), nullable=True)
     number = AppDb.Column(AppDb.Integer, nullable=False)  
     room = AppDb.Column(AppDb.String(65), nullable=False)
-    code = AppDb.Column(AppDb.String(65), nullable=False)
+    code = AppDb.Column(AppDb.String(65), nullable=False)  # todo: Unique?
 
     # relationship(s)
     lab = AppDb.relationship('Laboratory', backref='freezer', lazy=True)
 
     def __repr__(self):
-        return '<< Freezer: (number={0} || room={1} || lab={2} || code={3}) >>'\
-            .format(self.freezer, self.room, self.laboratory_id, self.code)
+        return '<< Freezer: (number={0} || room={1} || lab={2} || code={3}) >>' \
+            .format(self.number, self.room, self.laboratory_id, self.code)
--- api/resources/freezer_resource.py
+++ api/resources/freezer_resource.py
@@ -1,9 +1,13 @@
-from flask import current_app
+from datetime import datetime
+
+from flask import current_app, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_restful import fields, marshal, reqparse
 
 from api.models.database import BaseModel
 from api.models.freezer import Freezer
 from api.resources.base_resource import BaseResource
+from api.utils import format_and_lower_str, non_empty_int, non_empty_string
 
 
 class FreezerResource(BaseResource):
@@ -16,10 +20,17 @@ class FreezerResource(BaseResource):
     }
 
     def get(self):
-        freezer = Freezer.query.all()
-        data = marshal(freezer, self.fields)
-        return BaseResource.send_json_message(data, 200)
+        if request.headers.get('code') is not None:
+            code = format_and_lower_str(request.headers['code'])()
+            freezer = FreezerResource.get_freezer(code)
+            data = marshal(freezer, self.fields)
+            return BaseResource.send_json_message(data, 200)
+        else:
+            freezer = Freezer.query.all()
+            data = marshal(freezer, self.fields)
+            return BaseResource.send_json_message(data, 200)
 
+    @jwt_required
     def post(self):
         args = FreezerResource.freezer_args()
 
@@ -32,54 +43,67 @@ class FreezerResource(BaseResource):
 
         BaseModel.db.session.add(freezer)
         BaseModel.db.session.commit()
+        current_app.logger.info(
+            "New {0} created by {1} at {2}".format(freezer, get_jwt_identity(), datetime.now()))
         return BaseResource.send_json_message("Freezer Successfully Created", 201)
 
-    def put(self, code):
+    @jwt_required
+    def put(self):
+        args = FreezerResource.freezer_args()
+        code = format_and_lower_str(args[3])()
         freezer = FreezerResource.get_freezer(code)
 
-        if not freezer:
+        if freezer is None:
             return BaseResource.send_json_message("Freezer does not exist", 404)
-        args = FreezerResource.freezer_args()
 
-        if args[0] != freezer.laboratory_id or args[1] != freezer.number or args[2] != freezer.room or args[3] != freezer.code:
+        elif args[0] != freezer.laboratory_id or args[1] != freezer.number or args[2] != freezer.room or args[
+            3] != freezer.code:
             try:
                 freezer.laboratory_id = args[0]
                 freezer.number = args[1]
                 freezer.room = args[2]
                 freezer.code = args[3]
                 BaseModel.db.session.commit()
+                current_app.logger.info("{0} updated {1} to {2} at time={3}"
+                                        .format(get_jwt_identity(), freezer, freezer,
+                                                datetime.now()))
                 return BaseResource.send_json_message("Successfully updated Freezer", 202)
 
             except Exception as e:
                 current_app.logger.error(e)
                 BaseModel.db.session.rollback()
                 return BaseResource.send_json_message("Error while updating freezer", 500)
-        return BaseResource.send_json_message("No changes made", 304)
+        else:
+            return BaseResource.send_json_message("No changes made", 304)
 
-    def delete(self, code):
+    @jwt_required
+    def delete(self):
+        args = FreezerResource.freezer_args()
+        code = format_and_lower_str(args[3])()
         freezer = FreezerResource.get_freezer(code)
 
-        if not freezer:
+        if freezer is None:
             return BaseResource.send_json_message("Freezer does not exist", 404)
 
         BaseModel.db.session.delete(freezer)
         BaseModel.db.session.commit()
+        current_app.logger.info("{0} deleted {1} at {2}".format(get_jwt_identity(), freezer, datetime.now()))
         return BaseResource.send_json_message("Freezer deleted", 200)
 
     @staticmethod
     def freezer_args():
         parser = reqparse.RequestParser()
         parser.add_argument('laboratory', required=True)
-        parser.add_argument('number', required=True)
-        parser.add_argument('room', required=True)
-        parser.add_argument('code', required=True)
+        parser.add_argument('number', required=True, type=non_empty_int)
+        parser.add_argument('room', required=True, type=non_empty_int)
+        parser.add_argument('code', required=True, type=non_empty_string)
 
         args = parser.parse_args()
 
         laboratory = args['laboratory']
         number = args['number']
         room = args['room']
-        code =args['code']
+        code = format_and_lower_str(args['code'])()
 
         return [
             laboratory, number, room, code
