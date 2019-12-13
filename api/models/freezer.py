e75ac1e4048ebe8e5a6eb89dc62b829f355f34d6
jeffkim <jeffkim207@gmail.com>
2019-12-13 15:05:45 +0300

    Updated model to check for existence of a freezer

--- api/models/freezer.py
+++ api/models/freezer.py
@@ -5,12 +5,20 @@ class Freezer(BaseModel.db.Model):
     AppDb = BaseModel.db
     id = AppDb.Column(AppDb.Integer, primary_key=True)
     laboratory_id = AppDb.Column(AppDb.Integer, AppDb.ForeignKey('laboratory.id', ondelete='SET NULL'), nullable=True)
-    number = AppDb.Column(AppDb.Integer, nullable=False)  
+    number = AppDb.Column(AppDb.Integer, nullable=False)
     room = AppDb.Column(AppDb.String(65), nullable=False)
     code = AppDb.Column(AppDb.String(65), nullable=False)  # todo: Unique?
 
     # relationship(s)
     lab = AppDb.relationship('Laboratory', backref='freezer', lazy=True)
+
+    @staticmethod
+    def freezer_exists(code):
+        if Freezer.query.filter(
+                Freezer.code == code
+        ).first():
+            return True
+        return False
 
     def __repr__(self):
         return '<< Freezer: (number={0} || room={1} || lab={2} || code={3}) >>' \
