2323caf17dc788f6856ea915f7f48b84705ef453
jeffkim <jeffkim207@gmail.com>
2020-01-07 12:55:35 +0300

    Created faker function to help in massive data generation in models.

---
+++ api/resources/faker_resource.py
@@ -0,0 +1,53 @@
+from flask import request
+
+from api.models import *
+from api.resources.base_resource import BaseResource
+from api.utils import format_and_lower_str, faker
+
+
+class FakeDataResource(BaseResource):
+    def post(self):
+        count = int(request.args.get('count'))
+        model = format_and_lower_str(request.args.get('model'))()
+
+        if model == 'box':
+            db_model = Box
+            return faker(count, db_model, model)
+        elif model == 'chamber':
+            db_model = Chamber
+            return faker(count, db_model, model)
+        elif model == 'freezer':
+            db_model = Freezer
+            return faker(count, db_model, model)
+        elif model == 'lab' or 'laboratory':
+            db_model = Laboratory
+            return faker(count, db_model, model)
+        elif model == 'publication':
+            db_model = Publication
+            return faker(count, db_model, model)
+        elif model == 'quantity type':
+            db_model = QuantityType
+            return faker(count, db_model, model)
+        elif model == 'rack':
+            db_model = Rack
+            return faker(count, db_model, model)
+        elif model == 'role':
+            db_model = Role
+            return faker(count, db_model, model)
+        elif model == 'sample':
+            db_model = Sample
+            return faker(count, db_model, model)
+        elif model == 'security level':
+            db_model = SecurityLevel
+            return faker(count, db_model, model)
+        elif model == 'theme':
+            db_model = Theme
+            return faker(count, db_model, model)
+        elif model == 'tray':
+            db_model = Tray
+            return faker(count, db_model, model)
+        elif model == 'user':
+            db_model = User
+            return faker(count, db_model, model)
+        else:
+            return
