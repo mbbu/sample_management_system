400f75357e22bfc7165f66a949cc94458facce34
jeffkim <jeffkim207@gmail.com>
2020-01-15 12:07:15 +0300

    Renamed resource and formatted text

--- api/resources/hello_world_resource.py
+++
@@ -1,6 +0,0 @@
-from api.resources.base_resource import BaseResource
-
-
-class HelloWorldResource(BaseResource):
-    def get(self):
-        return BaseResource.send_json_message('Hello, Welcome to MBBU Sample Management System!', 200)
