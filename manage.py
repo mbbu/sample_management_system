e872687a9ad88df4782b8c48d14a1ce868077d29
jeffkim <jeffkim207@gmail.com>
2020-01-15 13:04:01 +0300

    Fixed error with client dir location.

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 import os
 import subprocess
 
-from config import Config
 from flask_script import Manager
 from flask_socketio import SocketIO
 
+import client
 from app import create_app
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -22,20 +22,20 @@ def serve():
     """ Run Vue Development Server"""
     print('Starting the Vue development server... ')
     cmd = 'npm run serve'
-    _bash(cmd, cwd=Config.CLIENT_DIR)
+    _bash(cmd, cwd=client)
 
 
 def build():
     """ Build vue Application"""
     cmd = 'npm build'
-    _bash(cmd, cwd=Config.CLIENT_DIR)
+    _bash(cmd, cwd=client)
     print('Build completed')
 
 
 def lint():
     """ Lint Vue Application"""
     cmd = 'npm lint'
-    _bash(cmd, cwd=Config.CLIENT_DIR)
+    _bash(cmd, cwd=client)
     print('Lint completed')
 
 
