e3767770e6b4ed9a3461e7605462b6cc23d11631
jeffkim <jeffkim207@gmail.com>
2020-01-15 12:20:23 +0300

    Renamed file and updated router.js to allow for alias

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Box from './components/Box.vue';
 import Sample from './components/Sample.vue';
+import Index from './components/Index.vue';
 
 Vue.use(Router);
 
@@ -12,15 +13,22 @@ export default new Router({
      {
         path: '/box',
         name: 'Box',
-        component: Box,
-         },
-
-     {
-        path: '/sample',
-        name: 'Sample',
-        component: Sample,
+         component: Box,
      },
 
+         {
+             path: '/sample',
+             name: 'Sample',
+             alias: ['/samples'],
+             component: Sample,
+         },
+
+         {
+             path: '/',
+             name: 'home',
+             alias: ['/index', '/welcome'],
+             component: Index,
+         },
 
      ],
 });
