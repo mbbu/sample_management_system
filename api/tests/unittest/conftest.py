2b808db99f03e7ef62b924772351ddca81124d65
jeffkim <jeffkim207@gmail.com>
2020-02-05 17:21:16 +0300

    Reconfigured tests setup to use sqlite filesystem for doing tests. Fixed issue with database not being created.

--- api/tests/unittest/conftest.py
+++ api/tests/unittest/conftest.py
@@ -1,29 +1,30 @@
 import os
+import sqlite3
 import tempfile
+from sqlite3 import Error
 
 import pytest
+from flask import current_app
 
 from api import create_app
 from api.config import TestConfig
-from api.models.database import BaseModel
 
 
 @pytest.fixture
 def app():
     """create and configure a new app instance for each test"""
     # create the app with common test config
-    db_path = os.getcwd()
-    db_fd, db_name = tempfile.mkstemp(dir=db_path, suffix='.db')
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    db_fd, db_name = tempfile.mkstemp(dir=base_dir, suffix='.db')
 
     app = create_app(TestConfig.configs)
-    TestConfig.configs.update({'SQLALCHEMY_DATABASE_URI': 'sqlite:////' + db_name})
+    TestConfig.configs.update({'SQLALCHEMY_DATABASE_URI': 'sqlite:///' + db_name})
 
     with app.app_context():
-        BaseModel.init_app(app)
+        create_database(db_name)
     yield app
-
     os.close(db_fd)
-    os.unlink(db_name)
+    os.unlink(db_fd)
 
 
 @pytest.fixture
@@ -36,3 +37,24 @@ def client(app):
 def runner(app):
     """A test runner for the app's Click commands."""
     return app.test_cli_runner()
+
+
+def create_database(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+
+        with current_app.open_resource("./models/schema/schema.sql", 'rb') as f:
+            conn.executescript(f.read().decode("utf8"))
+            if conn.executescript(f.read().decode("utf8")):
+                print("Success")
+            else:
+                print('failed')
+
+        print('DB Ready!')
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+            print('True and closed')
