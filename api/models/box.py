e0ef20dc7d27b138193679da46c6f19300d22f78
jeffkim <jeffkim207@gmail.com>
2020-02-13 10:47:10 +0300

    Modified box to added code, which is unique and used to search, update and delete a specific box record.

--- api/models/box.py
+++ api/models/box.py
@@ -6,9 +6,18 @@ class Box(BaseModel.db.Model):
     id = AppDb.Column(AppDb.Integer, primary_key=True)
     tray_id = AppDb.Column(AppDb.Integer, AppDb.ForeignKey('tray.id', ondelete='SET NULL'), nullable=True)
     label = AppDb.Column(AppDb.String(65), nullable=False)
+    code = AppDb.Column(AppDb.String(65), nullable=False, unique=True, index=True)
 
     # relationship(s)
     tray = AppDb.relationship('Tray', backref='box', lazy=True)
 
     def __repr__(self):
-        return '<< Box: (label={0} || tray={1}) >>'.format(self.label, self.tray_id)
+        return '<< Box: (label={0} || tray={1} || code={2}) >>'.format(self.label, self.tray_id, self.code)
+
+    @staticmethod
+    def box_exists(code):
+        if Box.query.filter(
+                Box.code == code
+        ).first():
+            return True
+        return False
