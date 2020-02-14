be0de84779974c803c041a93e4ec2c79dae9b8da
jeffkim <jeffkim207@gmail.com>
2020-02-14 15:36:02 +0300

    Tested the index route

--- api/tests/unittest/test_01_factory.py
+++ api/tests/unittest/test_01_factory.py
@@ -1,3 +1,5 @@
+from flask import json
+
 from api import create_app
 from api.config import TestConfig
 
@@ -11,3 +13,10 @@ def test_config():
 def test_home(client):
     response = client.get('/home')
     assert response.data == b'Hello, Welcome to M.B.B.U Sample Management System!'
+
+
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert data['message'] == 'Hello, Welcome to M.B.B.U Sample Management System!'
