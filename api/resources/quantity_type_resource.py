a9c66cb41eb96646089520d826fd8bb5bd2802af
jeffkim <jeffkim207@gmail.com>
2020-02-18 15:33:52 +0300

    Refactored quantity_type_resource.py

--- api/resources/quantity_type_resource.py
+++ api/resources/quantity_type_resource.py
@@ -5,7 +5,7 @@ from flask_restful import fields, marshal, reqparse
 from api import BaseResource, BaseModel
 from api.models import QuantityType
 from api.utils import non_empty_string, format_and_lower_str, log_delete, log_create, log_update, log_duplicate, \
-    has_required_request_params
+    has_required_request_params, standard_non_empty_string
 
 
 class QuantityTypeResource(BaseResource):
@@ -33,7 +33,7 @@ class QuantityTypeResource(BaseResource):
     @jwt_required
     def post(self):
         args = QuantityTypeResource.quantity_parser()
-        _id = format_and_lower_str(args['code'])
+        _id = args['code']
         name = args['name']
         description = args['description']
 
@@ -62,7 +62,7 @@ class QuantityTypeResource(BaseResource):
             return BaseResource.send_json_message("Quantity type not found", 404)
         else:
             args = QuantityTypeResource.quantity_parser()
-            _id = format_and_lower_str(args['code'])
+            _id = args['code']
             name = args['name']
             description = args['description']
 
@@ -99,7 +99,7 @@ class QuantityTypeResource(BaseResource):
     @staticmethod
     def quantity_parser():
         parser = reqparse.RequestParser()
-        parser.add_argument('code', required=True, type=non_empty_string)
+        parser.add_argument('code', required=True, type=standard_non_empty_string)
         parser.add_argument('name', required=True, type=non_empty_string)
         parser.add_argument('description')
 
