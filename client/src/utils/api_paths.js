f68b9e0af66d37ca4d736eb827b097d18268942a
jeffkim <jeffkim207@gmail.com>
2020-04-02 19:29:44 +0300

    Resource route to role

--- client/src/utils/api_paths.js
+++ client/src/utils/api_paths.js
@@ -4,3 +4,4 @@ export var security_level_resource = 'http://localhost:5000/security-level';
 export var quantity_type_resource = 'http://localhost:5000/quantity-type';
 export var lab_resource = 'http://localhost:5000/lab';
 export var freezer_resource = 'http://localhost:5000/freezer';
+export var role_resource = 'http://localhost:5000/role' || 'http://localhost:5000/roles';
