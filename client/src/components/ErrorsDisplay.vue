fc3565abb71e3c999ff3502664a74d34416f2baf
jeffkim <jeffkim207@gmail.com>
2020-04-15 22:01:28 +0300

    Sticky navbar class and Component for displaying errors

---
+++ client/src/components/ErrorsDisplay.vue
@@ -0,0 +1,20 @@
+<template>
+    <p v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+    <ul>
+        <li :key="index" v-for="(error, index) in errors">{{ error }}</li>
+    </ul>
+    </p>
+</template>
+
+<script>
+    export default {
+        name: "ErrorsDisplay",
+        props: {
+            errors: {
+                type: Array,
+                required: false,
+            }
+        }
+    }
+</script>
