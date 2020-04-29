ec68c142a55f2ded6abdbdc1f91c95bdf1085656
jeffkim <jeffkim207@gmail.com>
2020-04-29 16:11:02 +0300

    Edit Profile form layout ready

---
+++ client/src/pages/UserProfile/EditProfileForm.vue
@@ -0,0 +1,229 @@
+<template>
+    <div>
+        <top-nav :page_title="page_title"></top-nav>
+        <br>
+        <mdb-card>
+            <div class="header pt-3 blue-gradient">
+                <mdb-card-header :data-background-color="dataBackgroundColor">
+                    <mdb-row class="d-flex justify-content-center">
+                        <h4 class="white-text mb-3 pt-3 title">Complete your profile</h4>
+                    </mdb-row>
+                </mdb-card-header>
+            </div>
+
+            <mdb-card-body class="mx-4 mt-4">
+                <form @submit.prevent="onSubmit">
+                    <div>
+                        <!--FIRST NAME-->
+                        <b-form-group :class="{ 'form-group--error': $v.user.firstName.$error }">
+                            <mdb-input icon="user" id="fName"
+                                       label="First name" type="text" v-model.trim="$v.user.firstName.$model"/>
+                            <div v-if="$v.user.firstName.$dirty">
+                                <div class="error" v-if="!$v.user.firstName.required">Field is
+                                    required
+                                </div>
+                                <div class="error" v-if="!$v.user.firstName.minLength">Name must
+                                    have at least {{$v.user.firstName.$params.minLength.min}}
+                                    letters.
+                                </div>
+                            </div>
+                        </b-form-group>
+
+                        <!--LAST NAME-->
+                        <b-form-group :class="{ 'form-group--error': $v.user.lastName.$error }">
+                            <mdb-input icon="user" id="lName"
+                                       label="Last name" type="text" v-model.trim="$v.user.lastName.$model"/>
+                            <div v-if="$v.user.lastName.$dirty">
+                                <div class="error" v-if="!$v.user.lastName.required">Field is
+                                    required
+                                </div>
+                                <div class="error" v-if="!$v.user.lastName.minLength">Name must have
+                                    at least {{$v.user.lastName.$params.minLength.min}} letters.
+                                </div>
+                            </div>
+                        </b-form-group>
+
+                        <!-- ROLE -->
+                        <!-- LIST POPULATED FROM ROLE RESOURCE -->
+                        <b-form-group class="form_input_margin">
+                            <i class="fas fa-user-cog"></i> Role: <br/>
+                            <ejs-dropdownlist
+                                    :dataSource='roleDataList'
+                                    :fields="fields"
+                                    :v-model="user.role"
+                                    id='dropdownlist'
+                                    placeholder='Select a role'
+                            ></ejs-dropdownlist>
+                        </b-form-group>
+
+                        <!--EMAIL-->
+                        <b-form-group :class="{ 'form-group--error': $v.user.email.$error }">
+                            <mdb-input icon="envelope" id="email"
+                                       label="Your email" type="email" v-model.trim="$v.user.email.$model"/>
+                            <div v-if="$v.user.email.$dirty">
+                                <div class="error" v-if="!$v.user.email.required">Field is
+                                    required
+                                </div>
+                                <div class="error" v-if="!$v.user.email.email">Email mismatch.
+                                    Example johndoe123@icipe.org
+                                </div>
+                            </div>
+                        </b-form-group>
+                    </div>
+
+                    <mdb-row class="d-flex align-items-center mb-4 mt-5">
+                        <mdb-col class="d-flex align-items-start" md="5">
+                            <div class="text-center">
+                                <button @click="deactivateAccount()"
+                                        class="btn btn-outline-danger btn-rounded"
+                                        type="button"> Deactivate Account
+                                    <i class="fas fa-user-lock"></i>
+                                </button>
+                            </div>
+                        </mdb-col>
+                        <mdb-col class="d-flex justify-content-end" md="7">
+                            <div class="text-center">
+                                <button @click="onSubmit" class="btn btn-outline-info btn-rounded"
+                                        type="button"> Update
+                                    <i class="fas fa-pencil-alt"></i></button>
+                            </div>
+                        </mdb-col>
+                    </mdb-row>
+                </form>
+            </mdb-card-body>
+        </mdb-card>
+    </div>
+</template>
+<script>
+    import {mdbCard, mdbCardBody, mdbCardHeader, mdbCol, mdbInput, mdbRow} from "mdbvue";
+    import TopNav from "@/components/TopNav";
+    import {
+        countDownTimer,
+        extractApiData,
+        getItemDataList,
+        getSelectedItem,
+        getStoredUserDetails,
+        getUserEmail,
+        secureStoreSetString,
+        showFlashMessage,
+        viewPassword
+    } from '../../utils/util_functions';
+    import {email, minLength, required} from "vuelidate/lib/validators";
+    import {role_resource, user_resource} from "../../utils/api_paths";
+    import axios from "axios";
+
+    export default {
+        name: "edit-profile-form",
+        props: {
+            dataBackgroundColor: {
+                type: String,
+                default: ""
+            }
+        },
+        data() {
+            return {
+                page_title: "Edit Profile",
+                roleDataList: [],
+                fields: {text: '', value: ''},
+                user: {
+                    firstName: '',
+                    lastName: '',
+                    role: '',
+                    email: ''
+                },
+            };
+        },
+        validations: {
+            user: {
+                firstName: {required, minLength: minLength(3)},
+                lastName: {required, minLength: minLength(3)},
+                email: {required, email},
+            }
+        },
+        methods: {
+            viewPassword,
+            onSubmit() {
+                this.$log.info("FORM SUBMIT METHOD CALLED!");
+                // stop here if form is invalid
+                this.$v.$touch();
+                if (this.$v.$invalid) {
+                    this.$log.info("FORM INVALID!");
+                    return;
+                }
+                this.$log.info("FORM VALID!");
+                // api call to create user.
+                this.updateUser(this.user);
+            },
+            onLoadPage() {
+                getItemDataList(role_resource).then(data => {
+                    let roleList = extractApiData(data);
+                    // this.$log.info("Role list json: ", JSON.stringify(roleList));
+
+                    // update local variables with data from API
+                    this.fields = roleList['fields'];
+                    for (let i = 0; i < roleList.items.length; i++) {
+                        this.roleDataList.push({
+                            'Code': roleList.items[i].Code,
+                            'Name': roleList.items[i].Name,
+                        });
+                    }
+                })
+
+                this.fillUserFormForUpdate()
+            },
+
+            fillUserFormForUpdate: function () {
+                // set user details
+                let userForUpdate = getStoredUserDetails()
+                this.$log.info("User for update", userForUpdate)
+                this.user.firstName = userForUpdate['first_name']
+                this.user.lastName = userForUpdate['last_name']
+                this.user.email = getUserEmail()
+
+                this.$log.info("User fields", this.user)
+            },
+
+            updateUser: function (user) {
+                let self = this;
+
+                this.user.role = getSelectedItem(this.roleDataList, this.user.role);
+                axios.put(user_resource, {
+                    first_name: user.firstName,
+                    last_name: user.lastName,
+                    email: user.email,
+                    role: user.role
+                })
+                    .then((response) => {
+                        // redirect after successful signUp
+                        if (response.status === 201) {
+                            showFlashMessage(self, 'success', 'User Updated', 'Redirecting you to home page in ' +
+                                countDownTimer(self, this.countDown, '/home') + " seconds");
+                            secureStoreSetString(response.data.message.token);
+                        }
+                    })
+                    .catch((error) => {
+                        this.$log.error(error);
+                        if (error.response) {
+                            if (error.response.status === 409) {
+                                showFlashMessage(self, 'error', error.response.data['message'], '');
+                            } else if (error.response.status === 400) {
+                                showFlashMessage(self, 'error', error.response.data['message'], 'Kindly refill the form');
+                            } else if (error.response.status === 500) {
+                                showFlashMessage(self, 'error', "Fatal Error", 'Admin has been contacted.');
+                            } else {
+                                showFlashMessage(self, 'error', error.response.data['message'], '');
+                            }
+                        }
+                    })
+
+            },
+            deactivateAccount() {
+            },
+        },
+        components: {mdbCard, mdbCardHeader, mdbCardBody, mdbRow, mdbCol, mdbInput, TopNav},
+        created() {
+            this.onLoadPage();
+        }
+    };
+</script>
+<style></style>
