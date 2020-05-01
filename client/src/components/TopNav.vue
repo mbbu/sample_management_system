b1c197202b420e93210e4463f163fd330eb7ba1f
jeffkim <jeffkim207@gmail.com>
2020-05-01 11:02:46 +0300

    Top Nav redesigned

--- client/src/components/TopNav.vue
+++ client/src/components/TopNav.vue
@@ -1,37 +1,100 @@
 <template>
-<div>
-    <b-navbar toggleable="lg" type="dark" variant="dark">
-        <b-navbar-nav class="item_title"><h1>{{ page_title }}</h1></b-navbar-nav>
+    <div>
+        <mdb-navbar class="top-nav" dark position="top" scrolling>
+            <mdb-navbar-brand href="#">
+                <router-link to="/index">
+                    <img :src="require('../assets/logo.png')" class="menu_logo"/>
+                </router-link>
+            </mdb-navbar-brand>
+            <mdb-navbar-toggler>
+                <mdb-navbar-nav>
+                    <mdb-nav-item class="item_title" href="#" waves-fixed><h2> {{ page_title }} </h2></mdb-nav-item>
+                </mdb-navbar-nav>
 
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
+                <mdb-form-inline>
+                    <mdb-input aria-label="Search" class="mr-sm-2" placeholder="Search" type="text"/>
+                    <mdb-btn class="my-0" outline="white" size="sm" type="submit">Search
+                        <i aria-hidden="true" class="fas fa-search"></i></mdb-btn>
+                </mdb-form-inline>
 
-            <b-nav-form>
-                <b-form-input size="sm" class="mr-sm-2"></b-form-input>
-                <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-            </b-nav-form>
+                <div v-if="isAuth === true">
+                    <mdb-dropdown class="nav-item item_title" tag="li">
+                        <mdb-dropdown-toggle
+                                navLink
+                                slot="toggle"
+                                tag="a"
+                                waves-fixed
+                        ><i class="fas fa-user fa-2x"></i> {{this.userName}}
+                        </mdb-dropdown-toggle
+                        >
+                        <mdb-dropdown-menu>
+                            <mdb-dropdown-item>
+                                <i class="far fa-id-badge fa-lg"></i>
+                                <router-link to="/user"> Profile</router-link>
+                            </mdb-dropdown-item>
+                            <mdb-dropdown-item>
+                                <i class="fas fa-sign-out-alt fa-lg"></i>
+                                <router-link @click.native="signOut" to="/home"> Sign Out</router-link>
+                            </mdb-dropdown-item>
+                        </mdb-dropdown-menu>
+                    </mdb-dropdown>
+                </div>
+                <div v-else-if="isAuth === false">
+                    <mdb-dropdown class="nav-item item_title" disabled="true" tag="li">
+                        <mdb-dropdown-toggle
+                                navLink
+                                slot="toggle"
+                                tag="a"
+                                waves-fixed
+                        ><i class="fas fa-user fa-2x"></i>User
+                        </mdb-dropdown-toggle>
+                        <mdb-dropdown-menu>
+                            <mdb-dropdown-item>
+                                <router-link to="/login">Login</router-link>
+                            </mdb-dropdown-item>
+                            <mdb-dropdown-item>
+                                <router-link to="/register">Sign Up</router-link>
+                            </mdb-dropdown-item>
+                        </mdb-dropdown-menu>
+                    </mdb-dropdown>
+                </div>
+            </mdb-navbar-toggler>
+        </mdb-navbar>
+        <div style="height: 18vh">
+            <div class="view intro-2">
+                <div class="full-bg-img">
+                    <div class="mask rgba-white-strong">
+                        <div class="container flex-center">
+                            <div class="dark-grey-text">
+                                <h2 class="font-weight-bold">
 
-            <b-nav-item-dropdown right>
-                <!-- Using 'button-content' slot -->
-                <template v-slot:button-content>
-                    <i class="fas fa-user fa-2x"></i> <em>User</em>
-                </template>
-                <b-dropdown-item>
-                    <i class="far fa-id-badge fa-lg"></i>
-                    <router-link to="/user"> Profile</router-link>
-                </b-dropdown-item>
-                <b-dropdown-item>
-                    <i class="fas fa-sign-out-alt fa-lg"></i>
-                    <router-link to="/home"> Sign Out</router-link>
-                </b-dropdown-item>
-            </b-nav-item-dropdown>
-        </b-navbar-nav>
-
-    </b-navbar>
-</div>
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+    import {getStoredUserDetails, isUserLoggedIn, logOutUser} from '../utils/util_functions';
+    import {
+        mdbBtn,
+        mdbDropdown,
+        mdbDropdownItem,
+        mdbDropdownMenu,
+        mdbDropdownToggle,
+        mdbFormInline,
+        mdbInput,
+        mdbNavbar,
+        mdbNavbarBrand,
+        mdbNavbarNav,
+        mdbNavbarToggler,
+        mdbNavItem
+    } from 'mdbvue'
+
     export default {
         name: 'top-nav',
         props: {
@@ -40,5 +103,34 @@
                 required: true,
             }
         },
+        data() {
+            return {
+                userName: null,
+                isAuth: null,
+            }
+        },
+        methods: {
+            getUser() {
+                this.isAuth = isUserLoggedIn()
+                if (this.isAuth === true) {
+                    let user = getStoredUserDetails()
+                    this.userName = user['first_name']
+                } else {
+                    this.userName = null
+                }
+            },
+
+            signOut() {
+                return logOutUser(this)
+            }
+        },
+        components: {
+            mdbInput, mdbNavbar, mdbNavbarBrand, mdbNavbarToggler, mdbNavbarNav, mdbNavItem,
+            mdbDropdown, mdbDropdownMenu, mdbDropdownItem, mdbDropdownToggle, mdbFormInline,
+            mdbBtn
+        },
+        created() {
+            this.getUser()
+        }
     }
 </script>
