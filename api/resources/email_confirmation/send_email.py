bcdc3c0d2081288f2a58f2195cd8c546740527e3
jeffkim <jeffkim207@gmail.com>
2020-05-11 11:40:47 +0300

    Added Threading to send email to allow for asynchronous email sending.

--- api/resources/email_confirmation/send_email.py
+++ api/resources/email_confirmation/send_email.py
@@ -1,15 +1,22 @@
+from threading import Thread
+
 from flask import current_app
 from flask_mail import Message, Mail
 
 from api.config import BaseConfig
 
 
+def send_async_email(application, message):
+    with application.app_context():
+        mail = Mail(current_app)
+        mail.send(message)
+
+
 def send_email(to, subject, template):
-    mail = Mail(current_app)
     msg = Message(
         subject,
         recipients=[to],
         html=template,
         sender=BaseConfig.ADMINS
     )
-    mail.send(msg)
+    Thread(target=send_async_email, args=(current_app._get_current_object(), msg)).start()
