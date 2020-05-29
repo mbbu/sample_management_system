09292e08121aa0d05a50d5752abe611282bc7999
jeffkim <jeffkim207@gmail.com>
2020-05-29 11:18:51 +0300

    Refactored is_theme_admin decorator to use already existing is_sys_admin function

--- api/resources/decorators/user_role_decorators.py
+++ api/resources/decorators/user_role_decorators.py
@@ -39,8 +39,7 @@ def is_theme_admin(theme_admin_restricted_func):
     def wrapper(*args, **kwargs):
         user_id = get_user_by_email(get_jwt_identity()).id
         theme_admin = User.query.join(Role).filter(User.id == user_id, Role.code == THEMEADMIN).first()
-        admin = User.query.join(Role).filter(User.id == user_id, Role.code == SYSADMIN).first()  # todo
-        if not (theme_admin or admin):
+        if not theme_admin or not is_sys_admin:
             return BaseResource.send_json_message(FORBIDDEN_FUNCTION_ACCESS_RESPONSE,
                                                   FORBIDDEN_FUNCTION_ACCESS_RESPONSE_CODE)
         return theme_admin_restricted_func(*args, **kwargs)
