c29cc71838b9aff5539a4ac87d2e07e927395cf8
jeffkim <jeffkim207@gmail.com>
2020-06-05 11:07:31 +0300

    Additional changes to bio-hazard model

--- api/models/bio_hazard_level.py
+++ api/models/bio_hazard_level.py
@@ -4,9 +4,9 @@ from api.models.database import BaseModel
 class BioHazardLevel(BaseModel.db.Model):
     AppDb = BaseModel.db
     id = AppDb.Column(AppDb.Integer, primary_key=True)
-    code = AppDb.Column(AppDb.String(65), nullable=True)
+    code = AppDb.Column(AppDb.String(65), nullable=False)
     name = AppDb.Column(AppDb.String(65), nullable=True)
-    description = AppDb.Column(AppDb.String(65), nullable=True)
+    description = AppDb.Column(AppDb.Text, nullable=True)
 
     # relationship(s) defined here
 
