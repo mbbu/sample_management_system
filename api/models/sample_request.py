ccc3f7bad128887d70fe49bae54a662cc4f2dd3d
jeffkim <jeffkim207@gmail.com>
2020-06-10 14:32:00 +0300

    added column to record the amount that was approved, in case it differs from the requested amount

--- api/models/sample_request.py
+++ api/models/sample_request.py
@@ -6,13 +6,15 @@ from api.models.database import BaseModel
 class SampleRequest(BaseModel.db.Model):
     AppDb = BaseModel.db
     id = AppDb.Column(AppDb.Integer, primary_key=True)
-    user = AppDb.Column(AppDb.Integer, AppDb.ForeignKey('users.id', ondelete='SET NULL'), nullable=False)
+    user = AppDb.Column(AppDb.Integer, AppDb.ForeignKey('users.id', ondelete='SET NULL'),
+                        nullable=False)  # todo: rename to requester
     sample = AppDb.Column(AppDb.Integer, AppDb.ForeignKey('sample.id', ondelete='SET NULL'), nullable=False)
     amount = AppDb.Column(AppDb.Integer, nullable=False)  # amount being requested
     request_date = AppDb.Column(AppDb.DateTime, nullable=False, default=datetime.now)
     response_date = AppDb.Column(AppDb.DateTime, nullable=True)
     status = AppDb.Column(AppDb.String(25), nullable=False)  # is the request pending, approved or rejected?
     notes = AppDb.Column(AppDb.Text, nullable=True)  # Any special issues about the sample request e.g. on reject
+    approved_amount = AppDb.Column(AppDb.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<< SampleRequest: (user={0} || sample={1} || amount={2} ' \
