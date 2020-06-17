49060f1d0f564e31065298d2e7c29ced6311de41
jeffkim <jeffkim207@gmail.com>
2020-06-17 11:30:51 +0300

    Updated function to save samples from redcap. Data matches the new form. Fields not found placed under todo

--- api/resources/redcap_requests/fetch_sample_resource.py
+++ api/resources/redcap_requests/fetch_sample_resource.py
@@ -47,7 +47,7 @@ class SaveSampleFromREDCap(BaseResource):
         else:
             # if (start_date or end_date or project) is None:
             # save all the samples to the db
-            SaveSampleFromREDCap.save_all_samples(sample_records)
+            return SaveSampleFromREDCap.save_all_samples(sample_records)
             # else:
             # save samples according to the filters passed
             # SaveSampleFromREDCap.save_samples_filtered_by_date(sample_records, start_date, end_date)
@@ -58,23 +58,15 @@ class SaveSampleFromREDCap(BaseResource):
             """
                 data returned from redcap:
                     {
-                    //"record_id":"1",
                     "name":"andrewespira", ? user_id
                     "mail":"aespira@icipe.org", ? user_id
                     "theme":"1", -> theme_id how to know theme 
                     "project":"IBCARP", -> project
                     ??"sample_type":"1", -> sample_type how to know sample types
                     "sample_id":"bm01_c2_01", -> ? code
-                    //"lab":"1", -> ?
-                    //"room_id":"ml-05", -> ?
-                    //"freezer_id":"mbbu-005", -> ?
                     "box_id":"cd-07", -> box_id
-                    //"rack_id":"4",
-                    ??"amount_taken":"7", -> amount
                     "date_return":"2020-05-30 11:55",
-                    //"sample_request_form_complete":"2",
                     $"project_name":"ibcarp",
-                    //"project_id":"008",
                     "staff_in_charge":"andre espira", -> ? user_id
                     "collection_type":"gf", -> ? description
                     "longitude":"36.8968905","lat":"-1.2215215000000001", -> location_collected
@@ -83,6 +75,15 @@ class SaveSampleFromREDCap(BaseResource):
                     ??"amount":"400", -> amount
                     "condition":"safe", -> ? security_level
                     "level":"7",
+                    
+                    
+                    //"record_id":"1",
+                    //"rack_id":"4",
+                    //"sample_request_form_complete":"2",
+                    //"lab":"1", -> ?
+                    //"project_id":"008",
+                    //"room_id":"ml-05", -> ?
+                    //"freezer_id":"mbbu-005", -> ?
                     }         
             """
 
@@ -95,83 +96,72 @@ class SaveSampleFromREDCap(BaseResource):
             box = None if AttributeError else BoxResource.get_box(sample_record['box_id']).id
             theme = None if AttributeError else ThemeResource.get_theme(sample_record['theme']).id
             code = sample_record['sample_id']
-            amount = sample_record['amount']
-            location_collected = {'lat': sample_record['lat'], 'long': sample_record['longitude']}
+            amount = None if ValueError else int(sample_record['amount'])
+            location = {'lat': sample_record['lat'], 'long': sample_record['longitude']}
+            owner = sample_record['staff_in_charge']
 
-            print(user)
-            print(sample_type)
-            print(project)
-            print(box)
-            print(theme)
-            print(code)
-            print(amount)
-            print(location_collected)
-            print(type(location_collected))
+            # todo: fields-not-found
+            '''
+                 animal_species
+                 sample_description
+                 security_level
+                 QT
+                 SL
+                 Temp
+                 Retention
+            '''
+            if not Sample.sample_exists(code):
+                print("success if block")
+                sample = Sample(code=code, theme_id=theme, user_id=user, box_id=box,
+                                sample_type=sample_type, location_collected=location,
+                                project_owner=owner, amount=amount)
 
+                BaseModel.db.session.add(sample)
+                BaseModel.db.session.commit()
+                log_export_from_redcap(sample)
+                return BaseResource.send_json_message("Samples successfully fetched and saved", 201)
+            log_duplicate(Sample.query.filter(Sample.code == code).first())
+            continue
+        return BaseResource.send_json_message("Sample already exists", 409)
 
-        #     # user = int(sample['users'].strip() or 0) or None
-        #     animal_species = sample['source_sample']
-        #     _type = sample['sample_type']
-        #     description = sample['sa_description']
-        #     location = sample['loc_sample']
-        #     owner = sample['pi']
-        #     amount = int(sample['number_samples_collected'].strip() or 0) or None
-        #     box = int(sample['box_number'].strip() or 0) or None
-        #     theme = int(sample['theme'].strip() or 0) or None
-        #     security_level = int(sample['risk_level'].strip() or 0) or None
-        #     record_id = sample['identifier_sample']
-        #
-        #     if not Sample.sample_exists(record_id):
-        #         sample = Sample(code=record_id, theme_id=theme, user_id=user, box_id=box, animal_species=animal_species,
-        #                         sample_type=_type, sample_description=description, location_collected=location,
-        #                         project_owner=owner, amount=amount, security_level=security_level)
-        #
-        #         BaseModel.db.session.add(sample)
-        #         BaseModel.db.session.commit()
-        #         log_export_from_redcap(sample)
-        #     log_duplicate(Sample.query.filter(Sample.code == record_id).first())
-        #     return BaseResource.send_json_message("Sample already exists", 409)
-        # return BaseResource.send_json_message("Samples successfully fetched and saved", 201)
+    @staticmethod
+    def save_samples_filtered_by_date(sample_records, start_date, end_date):
+        days_count = end_date - start_date
 
-    # @staticmethod
-    # def save_samples_filtered_by_date(sample_records, start_date, end_date):
-    #     days_count = end_date - start_date
-    #
-    #     for day in range(days_count.days + 1):
-    #         day = start_date + timedelta(days=day)
-    #         print(day)
-    #
-    #         for sample in sample_records:
-    #             _date = format_str_to_date(sample['date'] or DATE_TIME_NONE)
-    #
-    #             if day == _date:
-    #                 user = int(sample['users'].strip() or 0) or None
-    #                 animal_species = sample['source_sample']
-    #                 _type = sample['sample_type']
-    #                 description = sample['sa_description']
-    #                 location = sample['loc_sample']
-    #                 owner = sample['pi']
-    #                 amount = int(sample['number_samples_collected'].strip() or 0) or None
-    #                 box = int(sample['box_number'].strip() or 0) or None
-    #                 theme = int(sample['theme'].strip() or 0) or None
-    #                 security_level = int(sample['risk_level'].strip() or 0) or None
-    #                 record_id = sample['identifier_sample']
-    #
-    #                 if not Sample.sample_exists(record_id):
-    #                     sample = Sample(code=record_id, theme_id=theme, user_id=user, box_id=box,
-    #                                     animal_species=animal_species, sample_type=_type,
-    #                                     sample_description=description,
-    #                                     location_collected=location, project_owner=owner, amount=amount,
-    #                                     security_level=security_level)
-    #
-    #                     BaseModel.db.session.add(sample)
-    #                     BaseModel.db.session.commit()
-    #                     log_export_from_redcap(sample)
-    #                 log_duplicate(Sample.query.filter(Sample.code == record_id).first())
-    #                 return BaseResource.send_json_message("Sample already exists", 409)
-    #     return BaseResource.send_json_message(
-    #         "Samples from date {0} to date {1} saved".format(start_date, end_date), 201)
-    #
+        for day in range(days_count.days + 1):
+            day = start_date + timedelta(days=day)
+            print(day)
+
+            for sample in sample_records:
+                _date = format_str_to_date(sample['date'] or DATE_TIME_NONE)
+
+                if day == _date:
+                    user = int(sample['users'].strip() or 0) or None
+                    animal_species = sample['source_sample']
+                    _type = sample['sample_type']
+                    description = sample['sa_description']
+                    location = sample['loc_sample']
+                    owner = sample['pi']
+                    amount = int(sample['number_samples_collected'].strip() or 0) or None
+                    box = int(sample['box_number'].strip() or 0) or None
+                    theme = int(sample['theme'].strip() or 0) or None
+                    security_level = int(sample['risk_level'].strip() or 0) or None
+                    record_id = sample['identifier_sample']
+
+                    if not Sample.sample_exists(record_id):
+                        sample = Sample(code=record_id, theme_id=theme, user_id=user, box_id=box,
+                                        animal_species=animal_species, sample_type=_type,
+                                        sample_description=description,
+                                        location_collected=location, project_owner=owner, amount=amount,
+                                        security_level=security_level)
+
+                        BaseModel.db.session.add(sample)
+                        BaseModel.db.session.commit()
+                        log_export_from_redcap(sample)
+                    log_duplicate(Sample.query.filter(Sample.code == record_id).first())
+                    return BaseResource.send_json_message("Sample already exists", 409)
+        return BaseResource.send_json_message(
+            "Samples from date {0} to date {1} saved".format(start_date, end_date), 201)
 
 
 """
