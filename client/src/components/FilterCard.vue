69553b092826b75d66cf5e514d4a03039d74f76d
jeffkim <jeffkim207@gmail.com>
2020-07-02 10:49:40 +0300

    Created a FilterCard.vue component. This will help modularize the filters and reduce code

---
+++ client/src/components/FilterCard.vue
@@ -0,0 +1,61 @@
+<template>
+    <mdb-card>
+        <details>
+            <summary class="pt-3 blue-gradient d-flex justify-content-center white-text"><h5>Filters</h5>
+            </summary>
+            <mdb-card-body>
+                <mdb-row>
+                    <div :key="possibleFilter" v-for="possibleFilter in allFilters">
+                        <mdb-col :md="` ${(12 / allFilters.length) }`" class="d-flex align-items-start">
+                            <ul :key="title" v-for="(values, title) in possibleFilter">
+                                <li><em>{{title}}</em>
+                                    <hr>
+                                </li>
+                                <li :key="filter" v-for="filter in values">
+                                    <label>
+                                        <input :checked="filters.includes(filter)"
+                                               @change="toggleFilter(filter)"
+                                               type="checkbox">
+                                        <span> {{ filter }}</span>
+                                    </label>
+                                </li>
+                            </ul>
+                        </mdb-col>
+                    </div>
+                </mdb-row>
+            </mdb-card-body>
+        </details>
+    </mdb-card>
+</template>
+
+<script>
+    import {mdbCard, mdbCardBody, mdbCol, mdbRow} from "mdbvue";
+    import EventBus from "./EventBus";
+
+    export default {
+        name: "FilterCard",
+        components: {mdbCard, mdbCardBody, mdbRow, mdbCol},
+
+        data() {
+            return {
+                filters: []
+            }
+        },
+
+        props: {
+            allFilters: {
+                required: true,
+            },
+        },
+
+        methods: {
+            toggleFilter: function (newFilter) {
+                this.filters = !this.filters.includes(newFilter)
+                    ? [...this.filters, newFilter]
+                    : this.filters.filter(filter => filter !== newFilter)
+
+                EventBus.$emit('filters', this.filters);
+            },
+        }
+    }
+</script>
