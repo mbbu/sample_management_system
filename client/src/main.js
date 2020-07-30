e655a7fb1a03e646ac3dbbd5101fd367b21dd47f
jeffkim <jeffkim207@gmail.com>
2020-07-30 16:33:32 +0300

    vue pagination plugin

--- client/src/main.js
+++ client/src/main.js
@@ -5,6 +5,7 @@ import {DropDownListPlugin} from "@syncfusion/ej2-vue-dropdowns";
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import Page from 'v-page';
 import Vuelidate from "vuelidate";
 import VueLogger from 'vuejs-logger';
 import FlashMessage from "@smartweb/vue-flash-message";
@@ -31,6 +32,10 @@ const flash_config = {
     icon: true
 };
 
+const pagination_config = {
+    language: 'en'
+}
+
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
 Vue.use(DropDownListPlugin);
@@ -38,11 +43,14 @@ Vue.use(VueAxios, axios);
 Vue.use(Vuelidate);
 Vue.use(VueLogger, options);
 Vue.use(FlashMessage, flash_config);
-Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.use(Loading);
+Vue.use(Page, pagination_config)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false;
 
+
 new Vue({
-  router,
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app');
