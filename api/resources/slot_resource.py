70637d1e111f1ef15d30d9cd1650f6384738dbc1
GeoffreyKimani <jeffkim207@gmail.com>
2020-09-30 11:01:11 +0300

    Updated Slot resource to convert the post function to a method that can be called from elsewhere

--- api/resources/slot_resource.py
+++ api/resources/slot_resource.py
@@ -14,36 +14,35 @@ class SlotResource(BaseResource):
     def get(self):
         return BaseResource.send_json_message("Coming up soon", 200)
 
-    @jwt_required
-    @is_theme_admin
-    def post(self, box, row, col):
-        box = int(box)
-        row = int(row)
-        col = int(col)
 
-        matrix = matrix_generator(row, col)
+def create_slots(box, row, col):
+    box = int(box)
+    row = int(row)
+    col = int(col)
 
-        for rc in matrix:
-            for pos in rc:
-                # auto-generate code
-                code = SlotResource.fake.ean(length=8)
+    matrix = matrix_generator(row, col)
 
-                if not Slot.slot_exists(code):
-                    try:
-                        slot = Slot(box_id=box, code=code, position=pos)
+    for rc in matrix:
+        for pos in rc:
+            # auto-generate code
+            code = SlotResource.fake.ean(length=8)
 
-                        BaseModel.db.session.add(slot)
-                        log_create(slot)
-                    except Exception as e:
-                        current_app.logger.error(e)
-                        BaseModel.db.session.rollback()
-                        return BaseResource.send_json_message("Error while adding slot", 500)
-                else:
-                    log_duplicate(Slot.query.filter(Slot.code == code).first())
-                    return BaseResource.send_json_message("Slot already exists", 409)
+            if not Slot.slot_exists(code):
+                try:
+                    slot = Slot(box_id=box, code=code, position=pos)
 
-        BaseModel.db.session.commit()  # commit once
-        return BaseResource.send_json_message("Slots successfully created", 201)
+                    BaseModel.db.session.add(slot)
+                    log_create(slot)
+                except Exception as e:
+                    current_app.logger.error(e)
+                    BaseModel.db.session.rollback()
+                    return BaseResource.send_json_message("Error while adding slot", 500)
+            else:
+                log_duplicate(Slot.query.filter(Slot.code == code).first())
+                return BaseResource.send_json_message("Slot already exists", 409)
+
+    BaseModel.db.session.commit()  # commit once
+    return BaseResource.send_json_message("Slots successfully created", 201)
 
 
 # function to create a matrix for the box to visualize the positions
