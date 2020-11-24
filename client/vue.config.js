147d211bab1ccbe6a3dfcf3901adf2d7f41fc462
GeoffreyKimani <jeffkim207@gmail.com>
2020-11-24 09:49:07 +0300

    development server exports

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,7 @@
-// module.exports = {
-//   devServer: {
-//     port: 80,
-//     host: '0.0.0.0',
-//     disableHostCheck: true
-//   }
-// }
+module.exports = {
+  devServer: {
+    port: 8080,
+    host: '0.0.0.0',
+    disableHostCheck: true
+  },
+}
