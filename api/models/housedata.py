ebab648ca3c425686c21612e9a823a7b51b270e1
GeoffreyKimani <jeffkim207@gmail.com>
2020-11-25 13:15:36 +0300

    Modified housedata form to mirror animal health field collection form.

--- api/models/housedata.py
+++ api/models/housedata.py
@@ -1,34 +1,34 @@
 from api.models.database import BaseModel
 
 
-class Housedata(BaseModel.db.Model):
+class AnimalHealthHouseData(BaseModel.db.Model):
     AppDb = BaseModel.db
     id = AppDb.Column(AppDb.Integer, primary_key=True)
-    user_id = AppDb.Column(AppDb.Integer, AppDb.ForeignKey('users.id', ondelete='SET NULL'), nullable=True)
-    education = AppDb.Column(AppDb.String(150), nullable=True)
-    employment = AppDb.Column(AppDb.String(150), nullable=True)
-    marital_status = AppDb.Column(AppDb.String(150), nullable=True)
-    number_of_people = AppDb.Column(AppDb.Integer, nullable=True)
-    number_of_children = AppDb.Column(AppDb.Integer, nullable=True)
-    number_of_animals = AppDb.Column(AppDb.Integer, nullable=True)
-    economic_activity = AppDb.Column(AppDb.String(250), nullable=True)
-    type_of_animals = AppDb.Column(AppDb.String(250), nullable=True)
-    farming_activities = AppDb.Column(AppDb.String(250), nullable=True)
-    social_economic_data = AppDb.Column(AppDb.Boolean, nullable=False, default=False)
-    code = AppDb.Column(AppDb.String(20), nullable=False, unique=True, index=True)  # id on badge
+    code = AppDb.Column(AppDb.String(20), nullable=False, unique=True, index=True)
+    farmer = AppDb.Column(AppDb.String(30), nullable=False)
+    cattle_id = AppDb.Column(AppDb.String(20), nullable=False)
+    cattle_name = AppDb.Column(AppDb.String(20), nullable=False)
+    cattle_color = AppDb.Column(AppDb.String(10), nullable=False)
+    cattle_sex = AppDb.Column(AppDb.String, nullable=False)
+    collar = AppDb.Column(AppDb.String, nullable=True)
+    pcv = AppDb.Column(AppDb.String, nullable=True)
+    diagnosis = AppDb.Column(AppDb.String, nullable=True)  # dx
+    treatment = AppDb.Column(AppDb.String, nullable=True)  # rx
+    cc_ml = AppDb.Column(AppDb.String, nullable=True)
+    notes = AppDb.Column(AppDb.Text, nullable=True)
 
     @staticmethod
-    def housedata_exists(code):
-        if Housedata.query.filter(
-                Housedata.code == code
+    def house_data_exists(code):
+        if HouseData.query.filter(
+                HouseData.code == code
         ).first():
             return True
         return False
 
     def __repr__(self):
-        return '<< Metadata: (education={0} || employment={1} || married={2} ||' \
-               'people={3} || children={4} || animals={5} || economic_activity={6} || ' \
-               'animatype={7} || farming_activities={8} || social_economic_data={9} || code={10} || user={11} ) >>' \
-            .format(self.education, self.employment, self.marital_status, self.number_of_people,
-                    self.number_of_children, self.number_of_animals, self.economic_activity,
-                    self.type_of_animals, self.farming_activities, self.social_economic_data, self.code, self.user_id)
+        return '{ House Data: (code={0} || farmer={1} || cattle_id={2} || cattle_name={3} ||' \
+               'color={4} || sex={5} || collar={6} || pcv={7} || dx={8} || rx={9} || ' \
+               'cc_ml={10} || notes={11} ) }' \
+            .format(self.code, self.farmer, self.cattle_id, self.cattle_name,
+                    self.cattle_color, self.cattle_sex, self.collar,
+                    self.pcv, self.diagnosis, self.treatment, self.cc_ml, self.notes)
