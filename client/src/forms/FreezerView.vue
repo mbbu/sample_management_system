29d33a41c6acef83f3dde9b83c11b1023697149a
GeoffreyKimani <jeffkim207@gmail.com>
2020-12-11 09:21:59 +0300

    racks of selected chamber gotten on chamber select.

--- client/src/forms/FreezerView.vue
+++ client/src/forms/FreezerView.vue
@@ -3,54 +3,52 @@
     <div id="container">
       <b-modal id="modal-freezer-view" title="Freezer View">
         <div class="row">
-          <div class="col">
-            <!--CHAMBER-->
-            <div class="col form-group">
-              <b-form-group id="chamber" label="Chamber:" label-for="form-chamber-input">
-                <ejs-dropdownlist
-                    id='chamber-dropdownlist' ref='chamber'
-                    :dataSource='chamberData' :enabled='chamberEnabled' :fields='chamberFields'
-                    placeholder='Select a chamber' @change="fillFormFieldsDependentOnChamber"
-                ></ejs-dropdownlist>
-              </b-form-group>
-            </div>
-
-            <!--RACK-->
-            <div class="col form-group">
-              <b-form-group id="rack" label="Rack:" label-for="form-rack-input">
-                <ejs-dropdownlist
-                    id='rack-dropdownlist' ref='rack'
-                    :dataSource='rackData' :enabled='rackEnabled' :fields='rackFields'
-                    placeholder='Select a rack' @change="fillFormFieldsDependentOnRack"
-                ></ejs-dropdownlist>
-              </b-form-group>
-            </div>
+          <!--CHAMBER-->
+          <div class="col form-group">
+            <b-form-group id="chamber" label="Chamber:" label-for="form-chamber-input">
+              <ejs-dropdownlist
+                  id='chamber-dropdownlist' ref='chamber'
+                  :dataSource='chamberData' :enabled='chamberEnabled' :fields='chamberFields'
+                  :v-model="chamberCode" placeholder='Select a chamber' @change="getChamberRacks"
+              ></ejs-dropdownlist>
+            </b-form-group>
           </div>
 
-          <div class="col">
-            <!--TRAY-->
-            <div class="col form-group">
-              <b-form-group id="tray" label="Tray:" label-for="form-tray-input">
-                <ejs-dropdownlist
-                    id='tray-dropdownlist' ref='tray'
-                    :dataSource='trayData' :enabled='trayEnabled' :fields='trayFields'
-                    placeholder='Select a tray' @change="fillFormFieldsDependentOnTray"
-                ></ejs-dropdownlist>
-              </b-form-group>
-            </div>
-
-            <!--BOX-->
-            <div class="col form-group">
-              <b-form-group id="box" label="Box:" label-for="form-box-input">
-                <ejs-dropdownlist
-                    id='box-dropdownlist' ref='box'
-                    :dataSource='boxData' :enabled='boxEnabled' :fields='boxFields'
-                    placeholder='Select a box' @change="fillFormFieldsDependentOnBox"
-                ></ejs-dropdownlist>
-              </b-form-group>
-            </div>
+          <!--RACK-->
+          <div class="col form-group">
+            <b-form-group id="rack" label="Rack:" label-for="form-rack-input">
+              <ejs-dropdownlist
+                  id='rack-dropdownlist' ref='rack'
+                  :dataSource='rackData' :enabled='rackEnabled' :fields='rackFields'
+                  placeholder='Select a rack' @change="fillFormFieldsDependentOnRack"
+              ></ejs-dropdownlist>
+            </b-form-group>
           </div>
         </div>
+
+        <div class="row">
+          <!--TRAY-->
+          <div class="col form-group">
+            <b-form-group id="tray" label="Tray:" label-for="form-tray-input">
+              <ejs-dropdownlist
+                  id='tray-dropdownlist' ref='tray'
+                  :dataSource='trayData' :enabled='trayEnabled' :fields='trayFields'
+                  placeholder='Select a tray' @change="fillFormFieldsDependentOnTray"
+              ></ejs-dropdownlist>
+            </b-form-group>
+          </div>
+
+          <!--BOX-->
+          <div class="col form-group">
+            <b-form-group id="box" label="Box:" label-for="form-box-input">
+              <ejs-dropdownlist
+                  id='box-dropdownlist' ref='box'
+                  :dataSource='boxData' :enabled='boxEnabled' :fields='boxFields'
+                  placeholder='Select a box' @change="fillFormFieldsDependentOnBox"
+              ></ejs-dropdownlist>
+            </b-form-group>
+          </div>
+      </div>
         <div class="justify-content-center">
 <!--          <div class="col justify-content-center">-->
             <rectangle></rectangle>
@@ -63,8 +61,10 @@
 
 <script>
 import Rectangle from "@/components/Rectangle";
-import {extractChamberData, getItemDataList} from "@/utils/util_functions";
-import { freezer_resource} from "@/utils/api_paths";
+import {extractChamberData, extractRackData,
+  getItemDataList, getSelectedItemCode}
+  from "@/utils/util_functions";
+import {chamber_resource, freezer_resource} from "@/utils/api_paths";
 import EventBus from "@/components/EventBus";
 export default {
   name: "FreezerView",
@@ -73,7 +73,7 @@ export default {
 
   data() {
     return{
-      freezerCode: '',
+      freezerCode: '', chamberCode: '',
       chamberData: [], chamberFields: {text: '', value: ''}, chamberEnabled: true,
       rackData: [], rackFields: {text: '', value: ''}, rackEnabled: false,
       trayData: [], trayFields: {text: '', value: ''}, trayEnabled: false,
@@ -90,6 +90,7 @@ export default {
 
   methods: {
     onLoadPage(){
+      // get freezer chamber
       getItemDataList(freezer_resource, {q: this.freezerCode}).then(data => {
         this.chamberData = []
         let chamberList = extractChamberData(data);
@@ -105,7 +106,25 @@ export default {
       })
     },
 
-    fillFormFieldsDependentOnChamber(){},
+    getChamberRacks(){
+      this.chamberCode = getSelectedItemCode('chamber-dropdownlist', this.chamberData)
+
+      // get racks of the selected chamber
+      getItemDataList(chamber_resource, {q: this.chamberCode}).then(data => {
+        this.rackData = []
+        let rackList = extractRackData(data);
+
+        // update local variables with data from API
+        this.rackFields = rackList['fields'];
+        for (let i = 0; i < rackList.items.length; i++) {
+          this.rackData.push({
+            'Code': rackList.items[i].Code,
+            'Name': rackList.items[i].Name,
+          });
+        }
+        this.rackEnabled = true
+      })
+    },
     fillFormFieldsDependentOnRack(){},
     fillFormFieldsDependentOnTray(){},
     fillFormFieldsDependentOnBox(){},
