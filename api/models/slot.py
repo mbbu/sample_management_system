3a31e9090c12681cedd000f68ac426fdf70b563c
GeoffreyKimani <jeffkim207@gmail.com>
2020-12-15 12:14:53 +0300

    On delete of box cascade to delete its slots also.

--- api/models/slot.py
+++ api/models/slot.py
@@ -10,7 +10,7 @@ class Slot(BaseModel.db.Model, SearchableMixin):
 
     AppDb = BaseModel.db
     id = AppDb.Column(AppDb.Integer, primary_key=True)
-    box_id = AppDb.Column(AppDb.Integer, AppDb.ForeignKey('box.id'), nullable=False)
+    box_id = AppDb.Column(AppDb.Integer, AppDb.ForeignKey('box.id', ondelete='CASCADE'), nullable=False)
     code = AppDb.Column(AppDb.String(65), nullable=False, unique=True, index=True)
     position = AppDb.Column(AppDb.JSON, nullable=True)
     available = AppDb.Column(AppDb.Boolean, nullable=False, default=True)
