d3f7e5a6eade38b0519720790d61e82de611876f
GeoffreyKimani <jeffkim207@gmail.com>
2021-01-25 17:09:18 +0300

    Updated Map component added library with selectable elements

--- client/src/pages/Map.vue
+++ client/src/pages/Map.vue
@@ -1,73 +1,29 @@
 <template>
-<div class="vue-world-map">
-  <top-nav :page_title="page_title"></top-nav>
-  <!-- FLASH MESSAGES -->
-  <FlashMessage :position="'right bottom'"></FlashMessage>
-  <Map/>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm-12">
+      <top-nav :page_title="page_title"></top-nav>
+
+      <!-- FLASH MESSAGES -->
+      <FlashMessage :position="'right bottom'"></FlashMessage>
+
+      <!-- MAP COMPONENT -->
+      <Map/>
+    </div>
+  </div>
 </div>
 </template>
 
 <script>
-  import chroma from 'chroma-js';
-  import Map from '../components/MapView';
-  import {getBaseCss, getCombinedCssString, getDynamicMapCss,} from '../styles/dynamic-map-css.js';
-  import TopNav from "../components/TopNav";
+  import Map from '@/components/MapView';
+  import TopNav from "@/components/TopNav";
 
   export default {
     components: {Map, TopNav},
-    watch: {
-      countryData() {
-        this.renderMapCSS();
-      },
+    data() {
+      return {
+        page_title: "Home",
+      };
     },
-    props: {
-      lowColor: {
-        type: String,
-        default: 'red',
-      },
-    highColor: {
-      type: String,
-      default: '#d83737',
-    },
-    countryData: {
-      type: Object,
-      required: true,
-    },
-    defaultCountryFillColor: {
-      type: String,
-      default: '#dadada',
-    },
-    countryStrokeColor: {
-      type: String,
-      default: '#ffffff',
-    },
-  },
-  data() {
-    return {
-      page_title: "Home",
-      node: document.createElement('style'),
-      chromaScale: chroma.scale([this.$props.lowColor, this.$props.highColor]),
-    };
-  },
-  methods: {
-    renderMapCSS() {
-      const baseCss = getBaseCss(this.$props);
-      const dynamicMapCss = getDynamicMapCss(this.$props.countryData, this.chromaScale);
-      this.$data.node.innerHTML = getCombinedCssString(baseCss, dynamicMapCss);
-    },
-  },
-  mounted() {
-    document.body.appendChild(this.$data.node);
-    this.renderMapCSS();
-  },
 };
 </script>
-
-<style>
-.vue-world-map {
-  height: 100%;
-}
-#map-svg {
-  height: 100%;
-}
-</style>
