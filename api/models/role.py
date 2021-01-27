b9ac5f327a3b8d4d0017574bce5a1b763e3e0f31
GeoffreyKimani <jeffkim207@gmail.com>
2021-01-27 10:15:08 +0300

    Unique code of role dropped

--- api/models/role.py
+++ api/models/role.py
@@ -4,7 +4,7 @@ from api.models.database import BaseModel
 class Role(BaseModel.db.Model):
     AppDb = BaseModel.db
     id = AppDb.Column(AppDb.Integer, primary_key=True)
-    code = AppDb.Column(AppDb.String(20), nullable=False, unique=True, index=True)  # id on badge
+    code = AppDb.Column(AppDb.String(20), nullable=False, unique=False, index=True)  # id on badge
     name = AppDb.Column(AppDb.String(50), nullable=False)  # predefined job
     description = AppDb.Column(AppDb.String(255), nullable=True)
 
